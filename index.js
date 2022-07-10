cf5e607c7e8995ba28806edc1877fe00a3643b09
asifalam515 <asibulalam515@gmail.com>
2022-07-10 20:41:29 +0600

    done

---
+++ index.js
@@ -0,0 +1,20 @@
+const express=require('express')
+const app=express()
+const simpleLogger=(req,res,next)=>{
+    console.log(`${req.url}-${req.method}-${new Date().toISOString()}`)
+  
+    next()
+}
+app.use(simpleLogger) //express will call himself
+
+app.get('/',(req,res)=>{
+
+  res.json({message:"Sweet Home!"})
+})
+app.get('/hello',(req,res,next)=>{
+    res.json({message:"Hello"})
+})
+
+app.listen(4000,()=>{
+    console.log("Server Running on 4000 port")
+})
